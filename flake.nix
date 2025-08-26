bcaabb4ebb880145033c324ce6431264ae6d4c2e
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-26 20:59:36 +0900

    chore: update environemnt

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            stack
+            ghc
             haskell-language-server
           ];
         };
