86a4c43f4cf9c5179f73598d033ecf251a65d6d6
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-02-14 00:40:44 +0900

    chore: update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
-  description = "haskell flake sample";
-
+  description = "profile card";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
